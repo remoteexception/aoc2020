3e425bf9bc2104ab1918d4cbee98b5f90d2de801
Jana Storch <jana.storch@dpdhl.com>
2021-11-09 19:44:15 +0100

    Minor refactoring

--- src/main/java/de/remoteexception/aoc/day/DayOne.java
+++ src/main/java/de/remoteexception/aoc/day/DayOne.java
@@ -1,20 +1,18 @@
 package de.remoteexception.aoc.day;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.NoSuchElementException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static de.remoteexception.aoc.util.ResourceUtil.lines;
 
 public class DayOne {
     private static final Logger log = Logger.getLogger(DayOne.class.getName());
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-        URI uri = ClassLoader.getSystemResource("dayone.txt").toURI();
-        Integer[] numbers = Files.readAllLines(Paths.get(uri)).stream()
+        Integer[] numbers = lines("dayone.txt")
             .map(Integer::valueOf).toArray(Integer[]::new);
 
         log.log(Level.INFO, "Product of two numbers: {0}", getProductOfTwo(numbers));
